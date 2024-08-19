152e25f90693f05fb7ebfeb1f1e834942c44ec47
Nigel Sprague <spraguenigel27@gmail.com>
2024-08-19 15:08:36 -0600

    buttons are linked

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log(':-)')
+
+let homeScore = 0
+let awayScore = 0
+
+function scorePoints() {
+  console.log('Team scores!')
+  console.log('Home: ' + 'Away: ')
+}
+
+function resetPoints() {
+  console.log('RESET')
+}
