c744e99b2499375651aa8411a39c17e85e1fbc72
Nigel Sprague <spraguenigel27@gmail.com>
2024-08-19 16:50:37 -0600

    functional

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ console.log(':-)')
 
 let homeScore = 0
 let awayScore = 0
+// let home1 = document.getElementById('home1')
+// let home3 = document.getElementById('home3')
+// let away1 = document.getElementById('away1')
+// let away3 = document.getElementById('away3')
 
-function scorePoints() {
-  console.log('Team scores!')
-  console.log('Home: ' + 'Away: ')
+function scoreHome(points) {
+  homeScore += points
+  console.log('Home Team scores!')
+  updateScores()
+}
+
+function scoreAway(points) {
+  awayScore += points
+  console.log('Away Team Scores!')
+  updateScores()
+}
+
+function updateScores() {
+  document.getElementById('homeCard').innerText = homeScore
+  document.getElementById('awayCard').innerText = awayScore
+  console.log('Home: ' + homeScore + ' Away: ' + awayScore)
 }
 
 function resetPoints() {
+  homeScore = 0
+  awayScore = 0
+  document.getElementById('homeCard').innerText = homeScore
+  document.getElementById('awayCard').innerText = awayScore
   console.log('RESET')
 }
