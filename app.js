ff5967a69d1a2175b46fef1b6a75bf72ccdf6be7
Nigel Sprague <spraguenigel27@gmail.com>
2024-08-19 21:35:15 -0600

    refactor scoring function

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ let homeScore = 0
 let awayScore = 0
 
 function scorePoints(team, points) {
-  if (team == )}
-
-function scoreHome(points) {
-  homeScore += points
-  console.log('Home Team scores!')
-  updateScores()
+  if (team == 'home') {
+    homeScore += points
+    console.log('Home Team scores!')
+    updateScores()
+  } else if (team == 'away') {
+    awayScore += points
+    console.log('Away Team Scores!')
+    updateScores()
+  }
 }
 
-function scoreAway(points) {
-  awayScore += points
-  console.log('Away Team Scores!')
-  updateScores()
-}
+// function scoreHome(points) {
+//   homeScore += points
+//   console.log('Home Team scores!')
+//   updateScores()
+// }
+
+// function scoreAway(points) {
+//   awayScore += points
+//   console.log('Away Team Scores!')
+//   updateScores()
+// }
 
 function updateScores() {
   document.getElementById('homeCard').innerText = homeScore
@@ -33,6 +42,25 @@ function resetPoints() {
 
 function gameEnd() {
   if (homeScore >= 11) {
+    console.log('end')
+    document.getElementById('gameEnd').innerText = 'Home Team Wins!'
     document.getElementById('gameEnd')
+    element.classList.remove('d-none')
+    document.getElementById('reset')
+    element.classList.add('d-none')
+    // } else if (awayScore >= 11) {
+    //   document.getElementById('gameEnd').innerText = 'Away Team Wins!'
+    //   document.getElementById('gameEnd')
+    //   element.classList.remove('d-none')
+    //   document.getElementById('reset')
+    //   element.classList.add('d-none')
   }
+}
+
+function playAgain() {
+  document.getElementById('reset')
+  element.classList.remove('d-none')
+  document.getElementById('gameEnd')
+  element.classList.add('d-none')
+  resetPoints()
 }
